7d67c01176c1f2b67922554234fa82d6c57338a0
vaibhu4coding <vaibhu4coding@gmail.com>
2023-12-10 17:49:04 +0530

    Request mapping added

--- src/main/java/com/ecom/api/controllers/OrderController.java
+++ src/main/java/com/ecom/api/controllers/OrderController.java
@@ -1,7 +1,9 @@
 package com.ecom.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
 }
